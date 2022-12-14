445ffafd44558535a314fd8704da7d1c3cd209fd
mircea007 <mirceareb@gmail.com>
2022-12-14 22:20:37 +0200

    using gunicorn because builtin http server is not secure acording to documentation

--- server.py
+++ server.py
@@ -1,31 +1,13 @@
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 import json
 
-hostName = "localhost"
-serverPort = 8080
+ok_response_obj = [{ 'verdict': 'Accepted', 'time': 0, 'memory': 250 }] * 20
+ok_response = bytes( json.dumps( ok_response_obj ), "utf-8" )
 
-ok_response = bytes( json.dumps( [
-    { 'verdict': 'Accepted', 'time': 0, 'memory': 250 }
-] * 20 ), "utf-8" )
-
-class MyServer( BaseHTTPRequestHandler ):
-    def do_GET( self ):
-        self.send_response( 200 )
-        self.send_header( "Content-type", "text/json" )
-        self.end_headers()
-        self.wfile.write( ok_response )
-
-if __name__ == "__main__":        
-    webServer = HTTPServer( (hostName, serverPort), MyServer )
-    print( f"Server started http://{hostName}:{serverPort}" )
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print( "Server stopped." )
-
+def app( environ, start_response ):
+    status = '200 OK'
+    response_headers = [
+        ( 'Content-type', 'text/plain' ),
+        ( 'Content-Length', str( len( ok_response ) ) )
+    ]
+    start_response( status, response_headers )
+    return iter( [ok_response] )
