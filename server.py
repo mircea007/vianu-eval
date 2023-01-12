5459dfbe2b78a38e9caf2ca338cdc057f2aa263f
mircea007 <mirceareb@gmail.com>
2023-01-12 21:10:43 +0200

    get request body

--- server.py
+++ server.py
@@ -10,6 +10,20 @@ ok_response_obj = {
 ok_response = bytes( json.dumps( ok_response_obj ), "utf-8" )
 
 def app( environ, start_response ):
+    # https://stackoverflow.com/questions/530526/accessing-post-data-from-wsgi
+    body= b"{'source':'#include<stdio.h>\nint main(){\n  return 0;\n}'}"
+    try:
+        length= int( environ.get('CONTENT_LENGTH', '0') )
+    except ValueError:
+        length= 0
+    if length!=0:
+        body = environ['wsgi.input'].read(length)
+
+    req_json = json.loads( body )
+    source = req_json['source']
+
+    print( 'Got source for evaluation:\n' + source )
+
     status = '200 OK'
     response_headers = [
         ( 'Content-type', 'text/plain' ),
